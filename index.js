32793965b2fb12d85c95ffdd65c6b17ba76ece19
cono6783 <86134894+cono6783@users.noreply.github.com>
2022-01-09 21:09:34 +0000

    changed all !== to !=

--- index.js
+++ index.js
@@ -5,47 +5,48 @@ const client = new Discord.Client()
 var advtimer = "on"
 var sidestimer = "on"
 
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`)
 })
 
 //turning advtimer on or off
 client.on("message", msg => {
-  if(msg.content.startsWith("$advtimerset")) {
-    if(msg.content === "on" || "off") {
-    advsetting = msg.content.split("$advtimerset ")[1]
-    advtimer = advsetting
-    if(!advtimer) {
-    advtimer = "on"
-    }
-    if(advtimer !== "on") {
-      if(advtimer !== "off") {
+  if (msg.content.startsWith("$advtimerset")) {
+    if (msg.content === "on" || "off") {
+      advsetting = msg.content.split("$advtimerset ")[1]
+      advtimer = advsetting
+      if (!advtimer) {
         advtimer = "on"
       }
-    }
-    msg.channel.send(`Adventure Timer is ${advtimer}`)
+      if (advtimer != "on") {
+        if (advtimer != "off") {
+          advtimer = "on"
+        }
+      }
+      msg.channel.send(`Adventure Timer is ${advtimer}`)
     }
   }
 })
 
 //displaying the value of advtimer
 client.on("message", msg => {
-  if(msg.content === "$advtimer") {
+  if (msg.content === "$advtimer") {
     msg.reply(`Advtimer is ${advtimer}`)
   }
 })
 
 //turning sidestimer on or off
 client.on("message", msg => {
-  if(msg.content.startsWith("$sidestimerset")) {
-    if(msg.content === "on" || "off") {
+  if (msg.content.startsWith("$sidestimerset")) {
+    if (msg.content === "on" || "off") {
       sidessetting = msg.content.split("$sidestimerset ")[1]
       sidestimer = sidessetting
-      if(!sidestimer) {
-      sidestimer = "on"
+      if (!sidestimer) {
+        sidestimer = "on"
       }
-      if(sidestimer !== "on") {
-        if(sidestimer !== "off"){
+      if (sidestimer != "on") {
+        if (sidestimer != "off") {
           sidestimer = "on"
         }
       }
@@ -56,17 +57,18 @@ client.on("message", msg => {
 
 //displaying the value of sidestimer
 client.on("message", msg => {
-  if(msg.content === "$sidestimer") {
+  if (msg.content === "$sidestimer") {
     msg.reply(`Sidestimer is ${sidestimer}`)
   }
 })
 
 //Adventure Time!
 client.on("message", msg => {
-  if(msg.content === ";adv"){
-    if(advtimer == "on") {
+  if (msg.content === ";adv") {
+    if (advtimer == "on") {
       setTimeout(() => {
-        msg.reply('Adventure Time!')}, 15000)
+        msg.reply('Adventure Time!')
+      }, 15000)
     }
   }
 })
@@ -75,27 +77,31 @@ client.on("message", msg => {
 //This is long because it was pinging it self
 client.on("message", msg => {
   //forage
-  if(msg.content === ";forage"){
-    if(sidestimer == "on"){
+  if (msg.content === ";forage") {
+    if (sidestimer == "on") {
       setTimeout(() => {
-        msg.reply("Sides Are Ready!")}, 300000)
+        msg.reply("Sides Are Ready!")
+      }, 300000)
     }
   }
   //chop
-   if(msg.content === ";chop"){
-    if(sidestimer == "on"){
+  if (msg.content === ";chop") {
+    if (sidestimer == "on") {
       setTimeout(() => {
-        msg.reply("Sides Are Ready!")}, 300000)
+        msg.reply("Sides Are Ready!")
+      }, 300000)
     }
   }
   //mine
-   if(msg.content === ";mine"){
-    if(sidestimer == "on"){
+  if (msg.content === ";mine") {
+    if (sidestimer == "on") {
       setTimeout(() => {
-        msg.reply("Sides Are Ready!")}, 300000)
+        msg.reply("Sides Are Ready!")
+      }, 300000)
     }
   }
 })
+
 
 //starting server
 isaping()
